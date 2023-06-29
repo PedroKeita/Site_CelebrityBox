780d05ca67f5f679b317fe85f73472ed371c610f
PedroKeita <82671771+PedroKeita@users.noreply.github.com>
2023-06-28 22:07:46 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors'); // Mova esta linha para o início do arquivo
+const enviarEmailRouter = require('./routes/enviarEmail');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const aboutRouter = require('./routes/about');
+const contactRouter = require('./routes/contact');
+
+const app = express();
+
+// Configuração do mecanismo de visualização
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+app.use('/users', usersRouter);
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+// Rota para processar o envio do formulário de contato
+app.use('/enviar-email', enviarEmailRouter);
+
+app.use(express.static('public'));
+// Captura erros 404 e encaminha para o manipulador de erros
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Manipulador de erros
+app.use(function(err, req, res, next) {
+  // Define as variáveis locais, fornecendo apenas o erro em desenvolvimento
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Renderiza a página de erro
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Iniciar o servidor
+const PORT = 8080; // Definir a porta como 8080
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado em http://localhost:${PORT}`);
+});
---
+++ routes/enviarEmail.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const { MongoClient } = require('mongodb');
+
+// Configuração do banco de dados MongoDB
+const uri = 'mongodb+srv://dev:PedroKeita@pedrobd.qeibapn.mongodb.net/?retryWrites=true&w=majority'; // Substitua pela URL do seu banco de dados MongoDB
+const client = new MongoClient(uri);
+
+// Rota para receber os dados do formulário de contato e salvar no banco de dados
+router.post('/', async (req, res) => {
+  try {
+    const { name, email, phone, message } = req.body;
+
+    // Conectar ao banco de dados
+    await client.connect();
+
+    // Acessar a coleção de contatos
+    const collection = client.db('PedroBD').collection('contatos'); // Substitua 'nomedobanco' pelo nome do seu banco de dados e 'contatos' pelo nome da sua coleção
+
+    // Criar um novo documento de contato
+    const newContact = {
+      name,
+      email,
+      phone,
+      message
+    };
+
+    // Inserir o novo documento na coleção de contatos
+    await collection.insertOne(newContact);
+
+    // Fechar a conexão com o banco de dados
+    await client.close();
+
+    // Redirecionar para uma página de sucesso ou qualquer outra página desejada
+    res.redirect('/contact');
+  } catch (error) {
+    console.error('Erro ao salvar os dados no banco de dados:', error);
+    res.redirect('/erro');
+  }
+});
+
+module.exports = router;
